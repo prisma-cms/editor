1ff036052176bcafa0e99b2cab0528ac3ee90fbe
Fi1osof <n.lanets@modxclub.ru>
2018-11-16 22:27:40 +0300

    dev

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-class App extends Component {
+
+import {
+  Editor,
+  EditorState,
+} from 'draft-js';
+
+
+class PrismaEditor extends Component {
 
   static propTypes = {
+    readOnly: PropTypes.bool.isRequired,
+    spellCheck: PropTypes.bool.isRequired,
+  }
 
-  };
+
+  static defaultProps = {
+    readOnly: true,
+    spellCheck: true,
+  }
+
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+
+
+  }
+
+  onChange = (editorState) => {
+
+    return this.setState({
+      editorState
+    });
+
+  }
 
   render() {
+
+    const {
+      editorState,
+    } = this.state;
+
     return (
-      <div>
-        My awesome component
-      </div>
+      <Editor
+        editorState={editorState}
+        onChange={this.onChange}
+      />
     );
   }
 }
- 
-export default App;
+
+export default PrismaEditor;
